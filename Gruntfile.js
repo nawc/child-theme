4ab5f0e7037b8c299659f7dac41c2948127c477c
Justin Hartman <code@justinhartman.co>
2021-06-28 14:35:02 +0200

    refactor: New grunt-git commands to fix releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,12 @@
  *
  * @author     Justin Hartman <code@justinhartman.co>
  * @copyright  Copyright (c) 2021, Justin Hartman <https://justinhartman.co>
- * @version    1.0.0
+ * @version    1.1.0
  */
 module.exports = function(grunt) {
     grunt.loadNpmTasks('git-changelog');
     grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-git');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -15,15 +16,6 @@ module.exports = function(grunt) {
             options: {
                 files: ['package.json'],
                 updateConfigs: ['pkg', 'git_changelog'],
-                commit: true,
-                commitMessage: 'chore: Tag Version %VERSION%',
-                commitFiles: ['-a'],
-                createTag: true,
-                tagName: '%VERSION%',
-                tagMessage: 'chore(release): %VERSION%',
-                push: true,
-                pushTo: 'origin',
-                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
                 globalReplace: true,
                 prereleaseName: false,
                 metadata: '',
@@ -34,56 +26,87 @@ module.exports = function(grunt) {
             main: {
                 options: {
                     app_name : 'Changelog',
-                    branch : "main",
-                    repo_url : "https://github.com/nawc/child-theme",
-                    file : "docs/tags/<%= pkg.version %>.md",
-                    template : "docs/tags/templates/log_template.md",
-                    commit_template : "docs/tags/templates/log_commit_template.md",
+                    branch : 'main',
+                    repo_url : 'https://github.com/nawc/child-theme',
+                    file : 'docs/tags/<%= pkg.version %>.md',
+                    template : 'docs/tags/templates/log_template.md',
+                    commit_template : 'docs/tags/templates/log_commit_template.md',
+                    // tag : false,
                     sections : [
                         {
-                            "title": "Bug Fixes",
-                            "grep": "^fix"
+                            'title': 'Bug Fixes',
+                            'grep': '^fix'
                         },
                         {
-                            "title": "New Features",
-                            "grep": "^feat"
+                            'title': 'New Features',
+                            'grep': '^feat'
                         },
                         {
-                            "title": "Documentation",
-                            "grep": "^docs"
+                            'title': 'Documentation',
+                            'grep': '^docs'
                         },
                         {
-                            "title": "Breaking Changes",
-                            "grep": "^breaking"
+                            'title': 'Breaking Changes',
+                            'grep': '^breaking'
                         },
                         {
-                            "title": "Refactored Code",
-                            "grep": "^refactor"
+                            'title': 'Refactored Code',
+                            'grep': '^refactor'
                         },
                         {
-                            "title": "Style Changes",
-                            "grep": "^style"
+                            'title': 'Style Changes',
+                            'grep': '^style'
                         },
                         {
-                            "title": "Testing",
-                            "grep": "^test"
+                            'title': 'Testing',
+                            'grep': '^test'
                         },
                         {
-                            "title": "Core Updates",
-                            "grep": "^chore"
+                            'title': 'Core Updates',
+                            'grep': '^chore'
                         },
                         {
-                            "title": "Branches Merged",
-                            "grep": "^Merge branch"
+                            'title': 'Branches Merged',
+                            'grep': '^Merge branch'
                         },
                         {
-                            "title" : "Pull Requests Merged",
-                            "grep": "^Merge pull request"
+                            'title' : 'Pull Requests Merged',
+                            'grep': '^Merge pull request'
                         }
                     ]
                 }
             }
         },
+        gitcommit: {
+            your_target: {
+                options: {
+                    message: 'chore: Tag Version <%= pkg.version %>',
+                    description: 'Add files for tag <%= pkg.version %>.',
+                },
+                files: {
+                   src:  ['package.json', 'yarn.lock', 'style.css', 'docs/tags']
+                }
+            }
+        },
+        gittag: {
+            addtag: {
+                options: {
+                    tag: '<%= pkg.version %>',
+                    message: 'chore(release): Release Version %VERSION%',
+                }
+            }
+        },
+        gitpush: {
+            your_target: {
+                options: {
+                    remote: 'origin',
+                    branch: 'main',
+                    all: true,
+                    tags: true
+                }
+            }
+        }
     });
     grunt.registerTask('default', ['bump', 'git_changelog']);
+    grunt.registerTask('publish', ['gitcommit', 'gittag', 'gitpush']);
 };
